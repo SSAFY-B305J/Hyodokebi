fb841eaaacd3dab65aadd7ffa1784a9d79c311b1
우재하 <jaeha6049@naver.com>
2024-03-12 14:15:33 +0900

    S10P22B305-270 #done #comment Feat: 헤더 프로토타입 생성

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div>Hello HyoDokebi</div>;
+  return (
+    <>
+      <div>Hello HyoDokebi</div>
+    </>
+  );
 }
 
 export default App;
---
+++ front/src/components/header/MainHeader.jsx
@@ -0,0 +1,39 @@
+import hyoblin from "../../assets/hyoblin.png";
+import club from "../../assets/club.png";
+
+interface MainHeaderProps {
+  props: boolean;
+}
+
+export default function MainHeader({ props }: MainHeaderProps) {
+  return (
+    <div>
+      {props === false ? (
+        <div className="w-full h-[10vh] flex flex-row justify-between items-center p-4 border-b-2 bg-white box-border">
+          <div className="m-4">
+            <img src={hyoblin} alt="" />
+          </div>
+          <div className="flex gap-5   m-4">
+            <div className="text-2xl font-semibold">회원가입</div>
+            <div className="text-2xl font-semibold">로그인</div>
+          </div>
+        </div>
+      ) : (
+        <div className="w-full h-[10vh] flex flex-row justify-between items-center p-4 border-b-2 bg-white">
+          <div className="m-4">
+            <img src={hyoblin} alt="" />
+          </div>
+          <div className="flex gap-5   m-4">
+            <div>
+              <img src={club} alt="" />
+            </div>
+            <div>
+              <img src="" alt="" />
+              {/* 프로필 아이콘 */}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
