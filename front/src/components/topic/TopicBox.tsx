228b5748e1422ef8ca918af893d934b6cce27052
우재하 <jaeha6049@naver.com>
2024-03-13 11:23:42 +0900

    Feat: 대화주제추천 분기
    S10P22B305-271 #in-progress #comment 초기 컴포넌트와 추천 상태 컴포넌트 분기 및 진행

--- front/src/components/topic/TopicBox.tsx
+++ front/src/components/topic/TopicBox.tsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
+
 export default function TopicBox() {
+  const [recom, setRecom] = useState(false);
 
   return (
     <div className="w-[75vw] h-[66vh] bg-secondary rounded-3xl p-3 flex">
-      <div className="flex flex-col items-center justify-around w-full h-full">
-        <div className="text-2xl">
-          대화 주제를 추천 해드립니다!
+      {recom ? (
+        <div className="flex flex-col items-center justify-around w-full h-full">
+          <div className="text-2xl">오늘은 이 주제로 대화 해보세요</div>
+          <div className="text-2xl">대나무헬리콥터의 우수성</div>
+          <button>
+            또다시 추천
+            {/* ALERT 해당 버튼은 임시, 버튼 컴포넌트를 만들어 사용하며, 로그인, 비로그인에 따른 분리 */}
+          </button>
         </div>
-        <button> 
-          버튼
-          {/* ALERT 해당 버튼은 임시 */}
-        </button>
-      </div>
+      ) : (
+        <div className="flex flex-col items-center justify-around w-full h-full">
+          <div className="text-2xl">대화 주제를 추천 해드립니다!</div>
+          <button onClick={() => setRecom(true)}>
+            추천 받기
+            {/* ALERT 해당 버튼은 임시, 버튼 컴포넌트를 만들어 사용하며, 로그인, 비로그인에 따른 분리 */}
+          </button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
