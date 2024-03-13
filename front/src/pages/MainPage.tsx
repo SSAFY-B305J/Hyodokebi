a5314c87bd07d4c48662e29916dead5080076bd9
우재하 <jaeha6049@naver.com>
2024-03-13 13:55:43 +0900

    Feat: Mypage add
    S10P22B305-272 #in-progress

--- front/src/pages/MainPage.tsx
+++ front/src/pages/MainPage.tsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
+
 import MainHeader from "../components/header/MainHeader";
 import TopicBox from "../components/topic/TopicBox";
 
 export default function MainPage() {
 
+  const [isLogin, setIsLogin] = useState(false)
+  
   return (
     <div className="flex flex-col w-screen h-screen">
-      <MainHeader isLogin={true} />
+      <MainHeader isLogin={isLogin} />
       <div className="flex items-center justify-center flex-1">
-        <TopicBox />
+        <TopicBox isLogin={isLogin} />
       </div>
     </div> 
   )
