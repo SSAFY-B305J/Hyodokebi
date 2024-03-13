e71af17f3f8ea98976d2a742df44c3bb119de0b9
임지은 <ljieun01@naver.com>
2024-03-13 17:34:28 +0900

    Feat: Button 컴포넌트 기본 타입 생성
    S10P22B305-323

--- front/src/components/button/CommonButton.tsx
+++ front/src/components/button/CommonButton.tsx
@@ -1,7 +1,26 @@
-export default function CommonButton () {
+interface ButtonProps {
+  text: string;
+  varient?: string;
+}
+
+interface ButtonColorType {
+  [key: string]: string;
+}
+
+export default function CommonButton({
+  text,
+  varient = "filled",
+}: ButtonProps) {
+  const buttonColors: ButtonColorType = {
+    filled: "bg-primary hover:bg-primary-hover text-white",
+    outlined:
+      "bg-white border border-primary hover:border-secondary text-primary hover:text-secondary",
+    text: "text-black bg-white hover:bg-gray-200",
+  };
 
   return (
-    <>
-    </>
-  )
+    <button className={`px-5 py-2 rounded-md ${buttonColors[varient]}`}>
+      {text}
+    </button>
+  );
 }
