545f4811cfcd313726f9efac373233e7b05fe6a6
우재하 <jaeha6049@naver.com>
2024-03-13 12:29:41 +0900

    Feat: 대화주제 분기 버튼 작동 구현

--- front/src/components/topic/TopicBox.tsx
+++ front/src/components/topic/TopicBox.tsx
@@ -7,8 +7,13 @@ export default function TopicBox() {
     <div className="w-[75vw] h-[66vh] bg-secondary rounded-3xl p-3 flex">
       {recom ? (
         <div className="flex flex-col items-center justify-around w-full h-full">
-          <div className="text-2xl">오늘은 이 주제로 대화 해보세요</div>
+          <div className="text-2xl font-medium">오늘은 이 주제로 대화 해보세요</div>
           <div className="text-2xl">대나무헬리콥터의 우수성</div>
+          <div className="flex items-center justify-center w-3/4 bg-white h-1/6 rounded-3xl">
+            <div className="text-xl">
+              대나무헬리콥터의 rpm을 알고 계신가요?
+            </div>
+          </div>
           <button>
             또다시 추천
             {/* ALERT 해당 버튼은 임시, 버튼 컴포넌트를 만들어 사용하며, 로그인, 비로그인에 따른 분리 */}
