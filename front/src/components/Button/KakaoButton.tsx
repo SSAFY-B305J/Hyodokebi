c663ade9fe5c4e31a09a938d0720725288c57def
임지은 <ljieun01@naver.com>
2024-03-18 16:17:39 +0900

    Design: 카카오 버튼 컴포넌트 css에 justify-center 추가
    S10P22B305-530

--- front/src/components/Button/KakaoButton.tsx
+++ front/src/components/Button/KakaoButton.tsx
@@ -2,7 +2,7 @@ import kakao from "../../assets/kakao.png";
 
 export default function KakaoButton(props: { text: string }) {
   return (
-    <button className="flex items-center bg-[#FEE500] px-7 py-2 font-bold rounded-md">
+    <button className="flex justify-center items-center bg-[#FEE500] hover:bg-[#E4D01C] px-7 py-2 font-bold rounded-md">
       <img src={kakao} alt="" className="pr-2.5" />
       <span>{props.text}</span>
     </button>
