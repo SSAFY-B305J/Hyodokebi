1f58be423c819669f0ec945745e8176b7fe01c49
우재하 <jaeha6049@naver.com>
2024-03-18 13:46:40 +0900

    Feat: 기존 페이지 레이아웃 적용

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -8,6 +8,7 @@ import App from "./App";
 import ErrorPage from "./pages/ErrorPage";
 import MainPage from "./pages/MainPage";
 import Mypage from "./pages/Mypage";
+import VipPage from "./pages/VipPage";
 
 
 const root = ReactDOM.createRoot(
@@ -33,10 +34,16 @@ const router = createBrowserRouter([
         element : <Mypage />,
         children: [
           {
-            path : "vip/:vipId"
-
+            path : "vip/:vipId",
           }
         ]
+      },
+      {
+        path : "food/choice/:id",
+        element : <VipPage />
+        //TODO children을 통해 food 이하 라우터 배분할것, 현재 element는 테스트용
+        // TODO 이곳이 맞는지? 지민이한테 확인하기
+
       }
     ]
   }
