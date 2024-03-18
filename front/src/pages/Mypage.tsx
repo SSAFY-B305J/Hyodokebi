2c4c4ab4d08c7d5a079b5be45c80085b30b17d43
우재하 <jaeha6049@naver.com>
2024-03-18 13:14:47 +0900

    Feat: MainLayout.tsx 추가

--- front/src/pages/Mypage.tsx
+++ front/src/pages/Mypage.tsx
@@ -1,18 +1,11 @@
-import { useState } from "react"
-
-import MainHeader from "../components/header/MainHeader"
 import DoubleTab from "../components/tab/DoubleTab"
+import MainLayout from "../layouts/Mainlayout"
 
 export default function Mypage () {
 
-  const [isLogin, setIsLogin] = useState(false)
-  
   return (
-    <div className="flex flex-col w-screen h-screen">
-      <MainHeader isLogin={isLogin} />
-      <div className="flex items-center justify-center flex-1">
-        <DoubleTab />
-      </div>
-    </div> 
+    <MainLayout>
+      <DoubleTab />
+    </MainLayout>
   )
 }
