07b6d5f901072ba1aa83b17457bb4fac3e98362a
우재하 <jaeha6049@naver.com>
2024-03-19 16:57:57 +0900

    Feat: useLoginStore devtools

--- front/src/store/useLoginStore.tsx
+++ front/src/store/useLoginStore.tsx
@@ -1,8 +1,9 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware"
 
-const useLoginStore = create((set) => ({
+const useLoginStore = create(devtools((set) => ({
   isLogin: false,
   setIsLogin: () => set(() => ({ isLogin: true}))
   // 로그인 로직에 따른 설정
   
-}))
+})))
