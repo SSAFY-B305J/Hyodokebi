6bbf0bcb73465beff9b154828d7abf91baf950b1
우지민 <0542say@naver.com>
2024-03-19 17:58:41 +0900

    Design: 요소 배치

--- front/src/components/food/map/Mapmap.tsx
+++ front/src/components/food/map/Mapmap.tsx
@@ -4,7 +4,7 @@ export default function Mapmap() {
   return (
     <Map
       center={{ lat: 33.5563, lng: 126.79581 }} // 지도의 중심 좌표
-      style={{ width: "800px", height: "600px" }} // 지도 크기
+      style={{ width: "800px", height: "400px" }} // 지도 크기
       level={3} // 지도 확대 레벨
     ></Map>
   );
--- front/src/pages/FoodListPage.tsx
+++ front/src/pages/FoodListPage.tsx
@@ -1,31 +1,30 @@
-import MainHeader from "../components/header/MainHeader";
 import FoodList from "../components/food/FoodList";
 import MainLayout from "../layouts/Mainlayout";
 // import MapList from "../components/food/map/MapList";
 import Mapmap from "../components/food/map/Mapmap";
+import Button from "../components/Button/Button";
 
 export default function FoodListPage() {
   return (
     <MainLayout>
-      <div className="flex flex-col w-screen h-screen">
-        <MainHeader isLogin={true} />
-        <div className="flex border border-current">
-          <div className="flex border border-current">메뉴</div>
-          <div className="flex border border-current">
-            <div className="flex border border-current">지역</div>
-            <div>
-              <div className="flex border border-current">하나</div>
-              <div className="flex border border-current">둘</div>
-              <div className="flex border border-current">셋</div>
-            </div>
+      <div className="flex flex-col border border-current">
+        <div className="flex p-5 text-2xl font-semibold border border-current">
+          메뉴 프록스 자리
+        </div>
+        <div className="flex p-3 border border-current ">
+          <div className="flex border border-current">지역</div>
+          <div className="flex flex-row ">
+            <div className="flex border border-current">하나</div>
+            <div className="flex border border-current">둘</div>
+            <div className="flex border border-current">셋</div>
           </div>
-          <div className="flex border border-current">
-            <Mapmap />
-          </div>
-          <div>
-            <div className="flex border border-current">음식점 정보</div>
-            <div className="flex border border-current">버튼-저장하기</div>
-          </div>
+        </div>
+        <div className="flex border-current ">
+          <Mapmap />
+        </div>
+        <div className="flex flex-row justify-end">
+          <div className="flex border border-current grow">음식점 정보</div>
+          <div className="flex border border-current">버튼-저장하기</div>
         </div>
       </div>
     </MainLayout>
