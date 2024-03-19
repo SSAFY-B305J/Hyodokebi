c7efd4c90597401161c42579e88830555d9711e4
우지민 <0542say@naver.com>
2024-03-19 10:41:54 +0900

    Merge file

--- front/src/components/card/Card.tsx
+++ front/src/components/card/Card.tsx
@@ -1,19 +1,27 @@
 import CloseIcon from "@mui/icons-material/Close";
-import test from "../../assets/test.jpg"
+import test from "../../assets/test.jpg";
 
 export default function Card() {
   return (
     <div className="box-border flex flex-col justify-between w-1/4 p-3 m-3 border-2 border-gray-300 shadow-md h-5/6 rounded-xl ">
       {/* ALERT width와 height는 확인용으로 넣은 것으로 추후 수정. */}
       <div className="flex justify-end">
-        <CloseIcon onClick={()=> console.log("닫기")} />
+        <CloseIcon onClick={() => console.log("닫기")} />
       </div>
       <div className="flex justify-center w-full">
-        <img src={test} alt="empty" className="w-3/4" />
+        <img
+          src={test}
+          alt="empty"
+          className="w-3/4"
+        />
       </div>
       <div className="flex flex-col justify-center">
-        <div className="flex justify-center m-3 text-2xl font-semibold">제목</div>
-        <div className="flex justify-center m-3 font-medium text-gray-500">부제목</div>
+        <div className="flex justify-center m-3 text-2xl font-semibold">
+          제목
+        </div>
+        <div className="flex justify-center m-3 font-medium text-gray-500">
+          부제목
+        </div>
       </div>
     </div>
   );
