dd1039cf3d0947c3d2b612f99ac0763b828ce222
임지은 <ljieun01@naver.com>
2024-03-20 12:18:48 +0900

    Feat: SignupForm 컴포넌트 추가
    S10P22B305-536 #in-progress

--- front/src/components/user/SignupForm.tsx
+++ front/src/components/user/SignupForm.tsx
@@ -1,9 +1,9 @@
 import FormContainer from "./FormContainer";
 import hyoblin from "../../assets/hyoblin.png";
-import Button from "../Button/Button";
 import KakaoButton from "../Button/KakaoButton";
 import HorizontalDivider from "./HorizontalDivider";
 import { Link } from "react-router-dom";
+import ButtonAsset from "../Button/ButtonAsset";
 
 export default function SignupForm() {
   return (
@@ -14,9 +14,9 @@ export default function SignupForm() {
             <img src={hyoblin} alt="효도깨비" />
           </Link>
         </div>
-        <div className="flex flex-col my-24 [&>*]:mb-3">
+        <div className="flex flex-col my-20 [&>*]:mb-3">
           <div className="flex flex-col">
-            <Button text="로그인" size="lg" />
+            <ButtonAsset text="로그인" size="lg" />
             <HorizontalDivider text="또는" />
             <KakaoButton text="카카오 로그인하기" size="lg" />
           </div>
