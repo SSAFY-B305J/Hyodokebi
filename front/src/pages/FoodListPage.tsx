c87ec79fbfb9c1c5b1736e929893eb5f03a8af6c
우지민 <0542say@naver.com>
2024-03-20 13:18:07 +0900

    Design: 음식리스트 페이지 CSS. 기능 미포함S10P22B305-496

--- front/src/pages/FoodListPage.tsx
+++ front/src/pages/FoodListPage.tsx
@@ -7,24 +7,48 @@ import Button from "../components/Button/Button";
 export default function FoodListPage() {
   return (
     <MainLayout>
-      <div className="flex flex-col border border-current">
-        <div className="flex p-5 text-2xl font-semibold border border-current">
+      <div className="flex flex-col ">
+        <div className="flex p-3 pb-1 ml-2 text-3xl font-semibold ">
           메뉴 프록스 자리
         </div>
-        <div className="flex p-3 border border-current ">
-          <div className="flex border border-current">지역</div>
-          <div className="flex flex-row ">
-            <div className="flex border border-current">하나</div>
-            <div className="flex border border-current">둘</div>
-            <div className="flex border border-current">셋</div>
+        <div className="flex h-20 p-1 mb-1 ">
+          <div className="flex items-center justify-center w-1/4 m-2 mr-3 text-xl font-bold border border-2 text-primary border-primary">
+            지역
+          </div>
+          <div className="flex flex-row p-3 grow">
+            <div className="flex w-1/6 mr-3 ">
+              <button className="flex items-center justify-center w-full bg-white border drop-shadow-lg border-silver rounded-2xl w-10screen">
+                하나
+              </button>
+            </div>
+            <div className="flex w-1/6 mr-3 ">
+              <button className="flex items-center justify-center w-full bg-white border drop-shadow-lg border-silver rounded-2xl">
+                둘
+              </button>
+            </div>
+            <div className="flex w-1/6 ">
+              <button className="flex items-center justify-center w-full bg-white border drop-shadow-lg border-silver rounded-2xl">
+                셋
+              </button>
+            </div>
           </div>
         </div>
         <div className="flex border-current ">
           <Mapmap />
         </div>
-        <div className="flex flex-row justify-end">
-          <div className="flex border border-current grow">음식점 정보</div>
-          <div className="flex border border-current">버튼-저장하기</div>
+        <div className="flex flex-row justify-end justify-around mt-5 ">
+          <div className="flex justify-between m-2 border border-primary grow">
+            <div className="flex m-1 ml-4 font-bold ">성심당 부띠끄</div>
+            <div className="flex m-1 font-medium ">
+              대전광역시 중구 대종로 480 중구 대종로 480
+            </div>
+            <div className="flex m-1 mr-4 font-semibold ">010-0000-0000</div>
+          </div>
+          <div className="flex w-1/6 pl-3 pr-3">
+            <button className="flex items-center justify-center w-full h-full bg-white border drop-shadow-lg text-primary border-primary rounded-3xl">
+              저장하기
+            </button>
+          </div>
         </div>
       </div>
     </MainLayout>
