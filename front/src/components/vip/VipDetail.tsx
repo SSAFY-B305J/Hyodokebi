1e5d5d37f933159a7507d3f5e13aa763eb168728
우재하 <jaeha6049@naver.com>
2024-03-20 11:20:51 +0900

    Feat: VipDetail 디자인 구성, 데이터 위치 넣을 곳
    S10P22B305-509

--- front/src/components/vip/VipDetail.tsx
+++ front/src/components/vip/VipDetail.tsx
@@ -1,20 +1,35 @@
 import { useNavigate } from "react-router";
 
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import CommonButton from "../button/CommonButton";
 
 export default function VipDetail() {
-
   const navigate = useNavigate();
 
   return (
-    <div className="box-border flex flex-col justify-between flex-1 w-full">
+    <div className="box-border flex flex-col flex-1 w-full p-3">
       <div className="flex flex-row justify-between my-3">
-        <div className="flex text-2xl font-semibold">
-          VIP 님의 정보
-        </div>
+        <div className="flex text-2xl font-semibold">VIP 님의 정보</div>
         <ArrowBackIcon fontSize="large" onClick={() => navigate(-1)} />
       </div>
-      <div className="flex w-3/4 m-2"></div>
+      <div className="flex flex-col justify-center w-full h-full m-2">
+        <div className="flex flex-row justify-around w-2/3 h-1/2 ">
+          <img src="https://picsum.photos/128/128" alt="" />
+          <div className="flex flex-col justify-between">
+            <p className="font-semibold">닉네임 : {}</p>
+            <p className="font-semibold">나이대 : {}</p>
+            <p className="font-semibold">지역 : {}</p>
+            {/* TODO DB에서 얻은 정보 입력 */}
+          </div>
+        </div>
+        <div className="flex justify-end mt-3">
+          <CommonButton text="수정하기" />
+          {/* TODO 수정 페이지로 */}
+        </div>
+        <div>
+          {/* VIP 정보 */}
+        </div>
+      </div>
     </div>
   );
 }
