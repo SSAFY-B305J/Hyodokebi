09422baa3f6d798b082c02ffe5224b4a787244a0
임지은 <ljieun01@naver.com>
2024-03-22 16:07:40 +0900

    Feat: 더미데이터로 로그인 기능 추가, 에러 메시지 박스 추가
    S10P22B305-484 #done

--- front/src/components/user/LoginForm.tsx
+++ front/src/components/user/LoginForm.tsx
@@ -2,12 +2,49 @@ import hyoblin from "../../assets/hyoblin.png";
 import KakaoButton from "../Button/KakaoButton";
 import VerticalDivider from "./VerticalDivider";
 import FormContainer from "./FormContainer";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import TextField from "../common/TextField";
 import ButtonAsset from "../Button/ButtonAsset";
 import { KAKAO_AUTH_URL } from "../../modules/auth/kakaoAuth";
+import { useState } from "react";
+import WarningIcon from "@mui/icons-material/Warning";
+
+const loginData = {
+  id: "ssafy1234",
+  password: "ssafy1234",
+};
 
 export default function LoginForm() {
+  // 아이디, 비밀번호
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  // 로그인 실패 여부
+  const [hasError, setHasError] = useState(false);
+
+  const navigate = useNavigate();
+
+  function onInputIdHandler(e: React.FormEvent<HTMLInputElement>) {
+    const value = e.currentTarget.value;
+    setId(value);
+  }
+
+  function onInputPasswordHandler(e: React.FormEvent<HTMLInputElement>) {
+    const value = e.currentTarget.value;
+    setPassword(value);
+  }
+
+  function onClickLoginButtonHandler() {
+    if (id === loginData.id && password === loginData.password) {
+      alert("로그인 성공!");
+      setHasError(false);
+      navigate("/");
+      return;
+    }
+
+    setHasError(true);
+  }
+
   return (
     <FormContainer>
       <>
@@ -16,12 +53,34 @@ export default function LoginForm() {
             <img src={hyoblin} alt="효도깨비" />
           </Link>
         </div>
+        {hasError && (
+          <div className="flex items-center justify-center px-6 py-5 mb-6 border rounded-md border-secondary bg-secondary/70">
+            <WarningIcon color="error" className="mr-4" />
+            <span className="text-sm break-keep">
+              아이디 또는 비밀번호가 맞지 않습니다. 다시 확인해주세요.
+            </span>
+          </div>
+        )}
         <div className="*:mb-4">
-          <TextField labelVisible={false} placeholder="아이디" />
-          <TextField labelVisible={false} placeholder="비밀번호" />
+          <TextField
+            labelVisible={false}
+            placeholder="아이디"
+            value={id}
+            onInput={onInputIdHandler}
+          />
+          <TextField
+            labelVisible={false}
+            placeholder="비밀번호"
+            value={password}
+            onInput={onInputPasswordHandler}
+          />
         </div>
         <div className="flex flex-col my-8 [&>*]:mb-3">
-          <ButtonAsset text="로그인" size="lg" />
+          <ButtonAsset
+            text="로그인"
+            size="lg"
+            onClick={onClickLoginButtonHandler}
+          />
           <Link to={KAKAO_AUTH_URL}>
             <KakaoButton text="카카오 로그인하기" size="lg" />
           </Link>
