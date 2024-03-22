3ff7bc6feab151b115d9b61d3f859119c601e95f
dykim <kdayun@daum.net>
2024-03-22 17:12:38 +0900

    Fix: 추천 노래 리스트 조회 플라스크 연결
    S10P22B305-524 #in-progress

--- Back/dokebi/src/main/java/com/dokebi/dokebi/music/dto/MusicDto.java
+++ Back/dokebi/src/main/java/com/dokebi/dokebi/music/dto/MusicDto.java
@@ -2,11 +2,14 @@ package com.dokebi.dokebi.music.dto;
 
 import lombok.*;
 
+@ToString
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MusicDto {
 
     private int musicId;
+
+    private int musicYear;
 
     private String musicName;
 
@@ -17,8 +20,9 @@ public class MusicDto {
     private String musicLyrics;
 
     @Builder
-    public MusicDto(int musicId, String musicName, String musicSinger, String musicImg, String musicLyrics) {
+    public MusicDto(int musicId, int musicYear, String musicName, String musicSinger, String musicImg, String musicLyrics) {
         this.musicId = musicId;
+        this.musicYear = musicYear;
         this.musicName = musicName;
         this.musicSinger = musicSinger;
         this.musicImg = musicImg;
--- Back/dokebi/src/main/java/com/dokebi/dokebi/music/repository/MusicRepositoryCustomImpl.java
+++ Back/dokebi/src/main/java/com/dokebi/dokebi/music/repository/MusicRepositoryCustomImpl.java
@@ -1,7 +1,9 @@
 package com.dokebi.dokebi.music.repository;
 
 import com.dokebi.dokebi.music.entity.Music;
+import com.dokebi.dokebi.music.entity.QDisLikedMusic;
 import com.dokebi.dokebi.music.entity.QMusic;
+import com.dokebi.dokebi.music.entity.QSavedMusic;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
@@ -22,5 +24,25 @@ public class MusicRepositoryCustomImpl implements MusicRepositoryCustom {
                 .where(qmusic.musicYear.between(group[0], group[1]))
                 .fetch();
 
+    }
+
+    @Override
+    public long removeSaveMusic(int mid, int vid) {
+        QSavedMusic qSavedMusic = QSavedMusic.savedMusic;
+
+        return queryFactory.delete(qSavedMusic)
+                .where(qSavedMusic.music.musicId.eq(mid)
+                        .and(qSavedMusic.vip.vipId.eq(vid)))
+                .execute();
+    }
+
+    @Override
+    public long removeDisLikeMusic(int mid, int vid) {
+        QDisLikedMusic qDisLikedMusic = QDisLikedMusic.disLikedMusic;
+
+        return queryFactory.delete(qDisLikedMusic)
+                .where(qDisLikedMusic.music.musicId.eq(mid)
+                        .and(qDisLikedMusic.vip.vipId.eq(vid)))
+                .execute();
     }
 }
