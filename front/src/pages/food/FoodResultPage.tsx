fed13fb241e94302d5977a2806630cd334b9ac8b
우지민 <0542say@naver.com>
2024-03-25 13:57:02 +0900

    Rename: 완성형 레이아웃 형식으로 변경

--- front/src/pages/food/FoodResultPage.tsx
+++ front/src/pages/food/FoodResultPage.tsx
@@ -1,30 +1,10 @@
-import FoodTap from "../../components/food/FoodTap";
+import FoodResult from "../../components/food/FoodResult";
+import MainLayout from "../../layouts/Mainlayout";
 
-export default function FoodResult() {
+export default function FoodResultPage() {
   return (
-    <div className="flex flex-col mt-5">
-      <div className="flex justify-center text-3xl font-bold mb-7">
-        효도깨비가 추천하는 식사메뉴
-      </div>
-      <div className="flex">
-        <FoodTap />
-      </div>
-      <div className="flex justify-center mt-5 mb-5 text-xl font-semibold ">
-        메뉴를 누르시면 주변의 음식점의 위치를 알려드립니다
-      </div>
-      <div className="flex "></div>
-      <div className="flex mt-3 justify-evenly h-14 ">
-        <div className="flex w-1/4">
-          <button className="w-full font-semibold border-2 rounded-3xl border-primary text-primary">
-            다시 추천하기
-          </button>
-        </div>
-        <div className="flex w-1/4 ">
-          <button className="w-full font-semibold border-2 rounded-3xl border-primary text-primary">
-            처음으로
-          </button>
-        </div>
-      </div>
-    </div>
+    <MainLayout>
+      <FoodResult />
+    </MainLayout>
   );
 }
