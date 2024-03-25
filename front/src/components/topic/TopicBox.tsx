469ccb8effb5be7a6a67cec7428fdfbfe4c36122
우재하 <jaeha6049@naver.com>
2024-03-25 11:23:38 +0900

    Feat: 대화 주제 클릭 시 복사

--- front/src/components/topic/TopicBox.tsx
+++ front/src/components/topic/TopicBox.tsx
@@ -1,5 +1,6 @@
 import useRecommendStore from "../../store/useRecommendStore";
 import ButtonAsset from "../Button/ButtonAsset";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 
 interface TopicBoxProps {
   isLogin: boolean;
@@ -9,10 +10,21 @@ export default function TopicBox({ isLogin }: TopicBoxProps) {
   const { recom, setRecom } = useRecommendStore();
   const topicshow = Math.floor(Math.random() * 10);
   const show = Math.floor(Math.random() * 10);
-  const randomtopictest = `testtopic${topicshow}`
-  const randomtest = `test${show}`; 
+  const clickToClipboard = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text)
+      alert('대화 주제를 복사 했습니다.')
+    } catch (e) {
+      alert('복사에 실패했습니다.')
+    }
+  }
+  const getContent = () => {
+    return document.getElementById("question")?.innerHTML ?? '';
+  };
+
+  const randomtopictest = `testtopic${topicshow}`;
+  const randomtest = `test${show}`;
   // json 내부 배열 길이에 따라 다르게 할 필요 있음
-  
 
   return (
     <div className="w-[75vw] h-[66vh] bg-secondary rounded-3xl p-3 flex">
@@ -22,13 +34,18 @@ export default function TopicBox({ isLogin }: TopicBoxProps) {
             오늘은 이 주제로 대화 해보세요.
           </div>
           <div className="text-xl">{randomtopictest}</div>
-          <div className="flex items-center justify-center w-3/4 bg-white h-1/6 rounded-3xl">
-            <div className="text-xl">{randomtest}</div>
+          <div className="flex flex-row items-center justify-center w-3/4 gap-3 bg-white h-1/6 rounded-3xl">
+            <div className="flex justify-center text-xl" id="question">
+              {randomtest}123
+            </div>
+            <div className="flex" onClick={() => {clickToClipboard(getContent())}}>
+              <ContentCopyIcon color="disabled" />
+            </div>
           </div>
           {isLogin ? (
             <ButtonAsset
               text="다시 추천 받기"
-              onClick={()=>(window.location.reload())}
+              onClick={() => window.location.reload()}
             />
           ) : (
             <button
