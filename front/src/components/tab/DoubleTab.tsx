f565a0022d31cefa136ca7cd743675609311fb14
우재하 <jaeha6049@naver.com>
2024-03-25 17:15:41 +0900

    Feat: doubletab.tsx usestate 삭제 후 zustand로 대체

--- front/src/components/tab/DoubleTab.tsx
+++ front/src/components/tab/DoubleTab.tsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import useTabStore from "../../store/useTabStore";
 
 export default function DoubleTab() {
-  const [index, setIndex] = useState(0);
   const {tabIndex, setTabIndex} = useTabStore()
   // TODO 해당 인덱스를 전역관리 할지?
   // TODO 탭에서 정해진 인덱스를 데이터에도 출력하여 보여주는 방식을 취할지?
