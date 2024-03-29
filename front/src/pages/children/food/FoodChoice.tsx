95fa303265b57dd42d27264114293da366960b16
우지민 <0542say@naver.com>
2024-03-29 14:05:35 +0900

    Feat: 지역선택, vip 선택 기능 추가
    S10P22B305-647

--- front/src/pages/children/food/FoodChoice.tsx
+++ front/src/pages/children/food/FoodChoice.tsx
@@ -2,53 +2,97 @@ import UnderLine from "../../../components/common/UnderLine";
 import FoodTap from "../../../components/food/FoodTap";
 import MainLayout from "../../../layouts/Mainlayout";
 import ChoiceVip from "../../../components/card/ChoiceVip";
-import Dropdown from "../../../components/common/Dropdowns";
+import Dropdowns from "../../../components/common/Dropdowns";
+import { useEffect, useState } from "react";
+import cityData from "../../../json/AadministrativeDistrict.json";
+import ButtonAsset from "../../../components/Button/ButtonAsset";
+import { Link } from "react-router-dom";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Box from "@mui/material/Box";
 
 interface VipProps {
-  id: number;
-  name: string;
-  imagePath: string;
-  imageIndex: string;
+  vipId: number;
+  vipNickname: string;
+  vipBirth: number;
+  vipProfile: string;
+  vipAgeGroups: string;
 }
 
 const vipDataList: VipProps[] = [
   {
-    id: 1,
-    name: "John Doe",
-    imagePath: "/src/assets/test.jpg",
-    imageIndex: "0",
+    vipId: 1,
+    vipNickname: "미애",
+    vipBirth: 1960,
+    vipProfile: "/",
+    vipAgeGroups: "/",
   },
   {
-    id: 1,
-    name: "Jane Smith",
-    imagePath: "https://placehold.co/600x400",
-    imageIndex: "1",
+    vipId: 2,
+    vipNickname: "미에",
+    vipBirth: 1960,
+    vipProfile: "/",
+    vipAgeGroups: "/",
   },
   {
-    id: 1,
-    name: "John Doe2",
-    imagePath: "/path/to/image1.jpg",
-    imageIndex: "2",
+    vipId: 3,
+    vipNickname: "미이에",
+    vipBirth: 1960,
+    vipProfile: "/",
+    vipAgeGroups: "/",
   },
   {
-    id: 1,
-    name: "Jane Smith2",
-    imagePath: "/path/to/image2.jpg",
-    imageIndex: "3",
+    vipId: 4,
+    vipNickname: "에이미",
+    vipBirth: 1960,
+    vipProfile: "/",
+    vipAgeGroups: "/",
   },
 ];
-
-const items = [
-  { label: "Option 1", value: "option1" },
-  { label: "Option 2", value: "option2" },
-  { label: "Option 3", value: "option3" },
-];
-
-function handleSelect(value: string) {
-  console.log("Selected:", value);
-}
 
 export default function FoodChoice() {
+  const [SelectVipId, setSelectVipId] = useState<string>("");
+  ///
+  const [selectedSiDo, setSelectedSiDo] = useState<string>("");
+  const [siGunGuOptions, setSiGunGuOptions] = useState<string[]>([]);
+
+  // 중복을 제거한 고유한 시/도 목록을 추출
+  const uniqueSiDoList = Array.from(
+    new Set(cityData.map((city) => city["si-do"]))
+  );
+
+  // 시/도를 선택했을 때 호출되는 함수
+  const handleSiDoChange = (event: SelectChangeEvent) => {
+    const selectedSiDoValue = event.target.value;
+    setSelectedSiDo(selectedSiDoValue);
+
+    // 선택된 시/도에 해당하는 시/군/구를 필터링하여 설정
+    const filteredSiGunGuOptions = cityData
+      .filter((city) => city["si-do"] === selectedSiDoValue)
+      .map((city) => city["si-gun-gu"]);
+    setSiGunGuOptions(filteredSiGunGuOptions);
+  };
+
+  ///
+
+  const [vipList, setVipList] = useState<VipProps[]>(vipDataList);
+
+  const [sel, setSel] = useState<number>(0);
+
+  const handleSelect = (cardId: number) => {
+    setSel(cardId);
+  };
+
+  useEffect(() => {
+    // vip 리스트 가져오는 함수(api)
+    // const data = await ~~
+    // setSel(data[0].id);
+    //setSel 초기화
+    //setVipList 초기화
+  }, []);
+
   return (
     <MainLayout>
       <div>
@@ -66,18 +110,85 @@ export default function FoodChoice() {
               text="지역을 골라주세요"
               size="sm"
             />
-            {/* <div className="flex flex-row">
-              <Dropdown
-                category="시/도"
-                cities={items}
-                onSelect={handleSelect}
-              />
-              <Dropdown
-                text="시/군/구"
-                items={items}
-                onSelect={handleSelect}
-              />
-            </div> */}
+            <div className="flex flex-row m-5">
+              <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-autowidth-label">
+                    시/도
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    value={selectedSiDo}
+                    onChange={handleSiDoChange}
+                  >
+                    {uniqueSiDoList.map((siDo, index) => (
+                      <MenuItem
+                        key={index}
+                        value={siDo}
+                      >
+                        {siDo}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Box>
+              <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-autowidth-label">
+                    시/군/구
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    value={selectedSiDo}
+                  >
+                    {selectedSiDo &&
+                      siGunGuOptions.map((siGunGu, index) => (
+                        <MenuItem
+                          key={index}
+                          value={siGunGu}
+                        >
+                          {siGunGu}
+                        </MenuItem>
+                      ))}
+                  </Select>
+                </FormControl>
+              </Box>
+              <div>
+                <label htmlFor="si-do">시/도 선택:</label>
+                {/* <select
+                  id="si-do"
+                  value={selectedSiDo}
+                  onChange={handleSiDoChange}
+                >
+                  <option value="">시/도를 선택하세요</option>
+                  {uniqueSiDoList.map((siDo, index) => (
+                    <option
+                      key={index}
+                      value={siDo}
+                    >
+                      {siDo}
+                    </option>
+                  ))}
+                </select>
+                 */}
+
+                <label htmlFor="si-gun-gu">시/군/구 선택:</label>
+                <select id="si-gun-gu">
+                  {/* 시/도가 선택된 경우에만 시/군/구 옵션을 동적으로 생성 */}
+                  {selectedSiDo &&
+                    siGunGuOptions.map((siGunGu, index) => (
+                      <option
+                        key={index}
+                        value={siGunGu}
+                      >
+                        {siGunGu}
+                      </option>
+                    ))}
+                </select>
+              </div>
+            </div>
           </div>
           <div className="mt-5">
             <UnderLine
@@ -85,14 +196,32 @@ export default function FoodChoice() {
               size="sm"
             />
             <div className="flex flex-row justify-center">
-              {vipDataList.map((vipData, index) => (
-                <ChoiceVip
-                  key={index}
-                  VipProps={vipData}
-                />
+              {vipList.map((vip) => (
+                <div onClick={() => handleSelect(vip.vipId)}>
+                  <ChoiceVip
+                    key={vip.vipId}
+                    id={vip.vipId}
+                    name={vip.vipNickname}
+                    imagePath={vip.vipProfile}
+                    imageIndex={""}
+                    clicked={vip.vipId === sel}
+                  />
+                </div>
               ))}
             </div>
           </div>
+        </div>
+        <div className="flex justify-center mt-3">
+          <Link
+            to={`food/choice/${SelectVipId}`}
+            className="w-1/4 "
+          >
+            <ButtonAsset
+              text="다음"
+              variant="outlined"
+              className="w-full font-semibold border-2 rounded-3xl hover:border-white "
+            />
+          </Link>
         </div>
       </div>
     </MainLayout>
