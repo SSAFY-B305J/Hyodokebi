ba6bae316195a4bc3b09fcefb540b24780f6b5b7
임지은 <ljieun01@naver.com>
2024-03-29 14:56:27 +0900

    Refactor: 회원가입 페이지 리펙토링. 전역 상태 관리로 변경

--- front/src/pages/children/user/RegForm.tsx
+++ front/src/pages/children/user/RegForm.tsx
@@ -1,285 +1,57 @@
-import TextField from "../../../components/common/TextField";
-import ButtonAsset from "../../../components/Button/ButtonAsset";
-import InputAsset from "../../../components/common/InputAsset";
-import { ChangeEvent, FormEvent, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { postRegist } from "../../../apis/api/member";
+import TextField from "../../../components/common/TextField";
+import useRegistStore from "../../../store/useRegistStore";
+import ButtonAsset from "../../../components/Button/ButtonAsset";
+import { FormEvent } from "react";
 
-export default function RegForm() {
-  // 아이디, 비밀번호, 비밀번호 확인, 이메일, 이메일 확인, 닉네임
-  const [id, setId] = useState<string>("");
-  const [pw, setPw] = useState<string>("");
-  const [pwConfirm, setPwConfirm] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  // const [authCode, setAuthCode] = useState<string>("");
-  const [nickname, setNickname] = useState<string>("");
-
-  // 아이디 에러, 비밀번호 에러, 비밀번호 확인 에러, 이메일 에러, 이메일 확인 에러, 닉네임 에러
-  const [idError, setIdError] = useState<Error | undefined>(undefined);
-  const [pwError, setPwError] = useState<Error | undefined>(undefined);
-  const [pwCheckError, setPwCheckError] = useState<Error | undefined>(
-    undefined
-  );
-  const [emailError, setEmailError] = useState<Error | undefined>(undefined);
-  const [nicknameError, setNicknameError] = useState<Error | undefined>(
-    undefined
-  );
-
-  // const [AuthButtonState, setAuthButtonState] = useState<boolean>(false);
-  // const [isEmailAuthStep, setIsEmailAuthVisible] = useState<boolean>(false);
-
+export default function RegForm2() {
   const navigate = useNavigate();
+  const { ...regStore } = useRegistStore();
 
-  // TODO: 비밀번호 확인을 쓴 후 비밀번호를 치면 비밀번호 확인이 유효한 오류 해결하기
-
-  async function SubmitHandler(e: FormEvent<HTMLFormElement>) {
+  // form submit 이벤트 핸들러
+  async function handleSubmit(e: FormEvent) {
     e.preventDefault();
 
-    const idCheck = idCheckHandler(id);
-    const pwCheck = pwCheckHandler(pw);
-    const pwConfirmCheck = pwConfirmCheckHandler(pwConfirm);
-    const emailCheck = emailCheckHandler(email);
-    const nicknameCheck = nicknameCheckHandler(nickname);
+    // 모든 input 유효성 검사하기
+    const isValid = await regStore.getRegistValid();
 
-    if (idCheck && pwCheck && pwConfirmCheck && emailCheck && nicknameCheck) {
+    // 모든 input 검사 통과하면 회원가입 요청
+    // TODO: 로그인 에러 확인하기
+    if (isValid) {
       // 회원가입 성공
-      try {
-        const data = await postRegist(id, pw, email, nickname, "");
-        console.log(data);
-
-        alert("회원가입 성공");
-        navigate("/login");
-      } catch (error) {
-        console.error("RegForm Error:", error);
-        alert("회원가입 실패");
-      }
+      postRegist(
+        regStore.id,
+        regStore.password,
+        regStore.email,
+        regStore.nickname,
+        ""
+      )
+        .then(() => {
+          alert("회원가입 성공!");
+          navigate("/login");
+        })
+        .catch((error) => {
+          console.error("RegForm Error:", error);
+          alert("회원가입 실패");
+        });
     }
   }
 
-  // 아이디 input change 핸들러
-  function onChangeIdHandler(e: ChangeEvent<HTMLInputElement>): void {
-    const value = e.target.value;
-    setId(value);
-  }
-
-  // 아이디 유효성 검사
-  function idCheckHandler(id: string): boolean {
-    const error = new Error();
-
-    // 빈칸인 경우
-    if (id === "") {
-      error.name = "error";
-      error.message = "아이디를 입력해주세요.";
-      setIdError(error);
-      return false;
-    }
-
-    // 영어, 숫자 4글자 이상, 13글자 이하 조건에 맞지 않는 경우
-    const idRegex = /^[a-z\d]{4,13}$/;
-    if (!idRegex.test(id)) {
-      error.name = "error";
-      error.message = "아이디는 4 ~ 13자의 영소문자, 숫자만 입력 가능합니다.";
-      setIdError(error);
-      return false;
-    }
-
-    // 중복된 아이디인 경우
-    const response = {}; // 아이디 중복 검사 API 응답값
-    if (!response) {
-      error.name = "error";
-      error.message = "이미 사용 중인 아이디입니다.";
-      setIdError(error);
-      return false;
-    }
-
-    error.name = "valid";
-    error.message = "사용 가능한 아이디입니다.";
-    setIdError(error);
-
-    return true;
-  }
-
-  // 비밀번호 input change 핸들러
-  function onChangePwHandler(e: ChangeEvent<HTMLInputElement>): void {
-    const value = e.target.value;
-    setPw(value);
-  }
-
-  // 비밀번호 유효성 검사
-  function pwCheckHandler(pw: string): boolean {
-    const error = new Error();
-
-    // 빈칸인 경우
-    if (pw === "") {
-      error.name = "error";
-      error.message = "비밀번호를 입력해주세요.";
-      setPwError(error);
-      return false;
-    }
-
-    // 영어, 숫자, 특수문자 6글자 이상, 16글자 이하 조건에 맞지 않는 경우
-    const pwRegex = /^[a-zA-Z0-9!@*&-_*]{6,16}$/;
-    if (!pwRegex.test(pw)) {
-      error.name = "error";
-      error.message =
-        "비밀번호는 6 ~ 16자의 영문, 숫자, 특수문자(!@*&-_)만 입력 가능합니다.";
-      setPwError(error);
-      return false;
-    }
-
-    error.name = "valid";
-    error.message = "";
-    setPwError(error);
-
-    return true;
-  }
-
-  // 비밀번호 확인 input change 핸들러
-  function onChangePwConfirmHandler(e: ChangeEvent<HTMLInputElement>): void {
-    const value = e.target.value;
-    setPwConfirm(value);
-    pwConfirmCheckHandler(value);
-  }
-
-  // 비밀번호 확인 유효성 검사
-  function pwConfirmCheckHandler(pwConfirm: string): boolean {
-    const error = new Error();
-
-    // 빈칸인 경우
-    if (pwConfirm === "") {
-      error.name = "error";
-      error.message = "비밀번호를 다시 한 번 입력해주세요.";
-      setPwCheckError(error);
-      return false;
-    }
-
-    // 비밀번호가 다른 경우
-    if (pwConfirm !== pw) {
-      error.name = "error";
-      error.message = "비밀번호가 일치하지 않습니다.";
-      setPwCheckError(error);
-      return false;
-    }
-
-    error.name = "valid";
-    error.message = "";
-    setPwCheckError(error);
-
-    return true;
-  }
-
-  // 이메일 input change 핸들러
-  function onChangeEmailHandler(e: ChangeEvent<HTMLInputElement>): void {
-    const value = e.target.value;
-    setEmail(value);
-  }
-
-  // 이메일 유효성 검사
-  function emailCheckHandler(email: string): boolean {
-    const error = new Error();
-
-    // 빈칸인 경우
-    if (email === "") {
-      error.name = "error";
-      error.message = "이메일을 입력해주세요.";
-      setEmailError(error);
-      // setAuthButtonState(false);
-      return false;
-    }
-
-    // 이메일 형식이 아닌 경우
-    const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-z]+$/;
-    if (!emailRegex.test(email)) {
-      error.name = "error";
-      error.message = "이메일 형식이 옳바르지 않습니다.";
-      setEmailError(error);
-      // setAuthButtonState(false);
-      return false;
-    }
-
-    // 이메일 중복 검사
-    const response = {}; // 이메일 중복 검사 API 응답값
-    if (!response) {
-      const error = new Error();
-      error.name = "error";
-      error.message = "이미 사용 중인 이메일입니다.";
-      setEmailError(error);
-      return false;
-    }
-
-    // 이메일 형식이 맞으면 인증 버튼 활성화
-    // setAuthButtonState(true);
-
-    // 이메일 인증하지 않은 않은 경우
-    // if (!isEmailAuthStep) {
-    //   const error = new Error();
-    //   error.name = "error";
-    //   error.message = "이메일을 인증해주세요.";
-    //   setEmailError(error);
-    //   return false;
-    // }
-
-    error.name = "valid";
-    error.message = "";
-    setEmailError(error);
-
-    return true;
-  }
-
-  // // 이메일 인증의 '인증하기' 버튼 click 핸들러
-  // // 이메일 확인 TextField를 보여줌
-  // function onClickEmailAuthButton(): void {
-  //   setIsEmailAuthVisible(true);
-  //   setAuthButtonState(false);
-  // }
-
-  // // 이메일 확인 input change 핸들러
-  // function onChangeAuthCodeHandler(e: ChangeEvent<HTMLInputElement>): void {
-  //   const value = e.target.value;
-  //   setAuthCode(value);
-  // }
-
-  // 닉네임 input change 핸들러
-  function onChangeNicknameHandler(e: ChangeEvent<HTMLInputElement>): void {
-    const value = e.target.value;
-    setNickname(value);
-    nicknameCheckHandler(value);
-  }
-
-  // 닉네임 유효성 검사
-  function nicknameCheckHandler(nickname: string): boolean {
-    const error = new Error();
-
-    // 빈칸인 경우
-    if (nickname === "") {
-      error.name = "error";
-      error.message = "닉네임을 입력해주세요.";
-      setNicknameError(error);
-      return false;
-    }
-
-    // 닉네임 중복 검사
-    const response = {}; // 닉네임 중복 검사 API 응답값
-    if (!response) {
-      const error = new Error();
-      error.name = "error";
-      error.message = "이미 사용 중인 닉네임입니다.";
-      setNicknameError(error);
-      return false;
-    }
-
-    error.name = "valid";
-    error.message = "";
-    setNicknameError(error);
-
-    return true;
+  // 취소 버튼 클릭 이벤트 핸들러
+  // 페이지 이탈 전에 confirm 창 띄우기
+  function handleCancelButton() {
+    const confirm = window.confirm(
+      "이 페이지에서 나가시겠습니까?\n지금까지 작성된 글은 저장되지 않습니다."
+    );
+    if (confirm) navigate("/signup");
   }
 
   return (
     <div className="m-14">
       <form
         className="flex flex-col items-center w-[500px]"
-        onSubmit={SubmitHandler}
+        onSubmit={handleSubmit}
       >
         <h1 className="my-6 text-3xl font-bold">회원가입</h1>
         <div className="w-full *:mb-8">
@@ -287,20 +59,20 @@ export default function RegForm() {
           <TextField
             label="아이디"
             placeholder="아이디"
-            value={id}
-            onChange={onChangeIdHandler}
-            onBlur={() => idCheckHandler(id)}
-            error={idError}
+            value={regStore.id}
+            onInput={(e) => regStore.setId(e.currentTarget.value)}
+            onBlur={regStore.checkId}
+            error={regStore.idError}
           />
           {/* 비밀번호 */}
           <TextField
             type="password"
             label="비밀번호"
             placeholder="비밀번호"
-            value={pw}
-            onChange={onChangePwHandler}
-            onBlur={() => pwCheckHandler(pw)}
-            error={pwError}
+            value={regStore.password}
+            onInput={(e) => regStore.setPassword(e.currentTarget.value)}
+            onChange={regStore.checkPassword}
+            error={regStore.passwordError}
             autoComplete="off"
           />
           {/* 비밀번호 확인 */}
@@ -308,79 +80,42 @@ export default function RegForm() {
             type="password"
             label="비밀번호 확인"
             placeholder="비밀번호 확인"
-            value={pwConfirm}
-            onChange={onChangePwConfirmHandler}
-            // onBlur={(e) => pwCheckCheckHandler(e)}
-            error={pwCheckError}
+            value={regStore.passwordConfirm}
+            onInput={(e) => regStore.setPasswordConfirm(e.currentTarget.value)}
+            onChange={regStore.checkPasswordConfirm}
+            error={regStore.passwordConfirmError}
             autoComplete="off"
           />
+
           {/* 이메일 */}
-          <div className="relative w-full">
-            <label className="inline-block pb-1 font-bold">이메일</label>
-            <div className="flex">
-              <InputAsset
-                type="email"
-                placeholder="이메일"
-                value={email}
-                onChange={onChangeEmailHandler}
-                onBlur={() => emailCheckHandler(email)}
-              />
-              {/* <ButtonAsset
-                type="button"
-                text="인증하기"
-                size="sm"
-                className="ml-2"
-                disabled={!AuthButtonState}
-                onClick={onClickEmailAuthButton}
-              /> */}
-            </div>
-            <p className="w-full pt-1 text-xs">
-              {emailError && emailError.message}
-            </p>
-          </div>
-          {/* 인증번호 입력 - 위의 '인증하기' 버튼을 클릭하면 출력됨 */}
-          {/* {isEmailAuthStep && (
-            <div className="relative w-full">
-              <label className="inline-block pb-1 font-bold">
-                인증번호 입력
-              </label>
-              <div className="flex">
-                <InputAsset
-                  placeholder="인증번호 입력"
-                  value={authCode}
-                  onChange={onChangeAuthCodeHandler}
-                />
-                <ButtonAsset
-                  type="button"
-                  text="확인하기"
-                  size="sm"
-                  className="ml-2"
-                  onClick={() => setIsEmailAuthVisible(true)}
-                />
-              </div>
-              <p className="w-full pt-1 text-xs"></p>
-            </div>
-          )} */}
+          <TextField
+            type="email"
+            label="이메일"
+            placeholder="이메일"
+            value={regStore.email}
+            onInput={(e) => regStore.setEmail(e.currentTarget.value)}
+            onBlur={regStore.checkEmail}
+            error={regStore.emailError}
+          />
           {/* 닉네임 */}
           <TextField
             label="닉네임"
             placeholder="닉네임"
-            value={nickname}
-            error={nicknameError}
-            onChange={onChangeNicknameHandler}
-            onBlur={() => nicknameCheckHandler(nickname)}
+            value={regStore.nickname}
+            onInput={(e) => regStore.setNickname(e.currentTarget.value)}
+            onBlur={regStore.checkNickname}
+            error={regStore.nicknameError}
           />
         </div>
         {/* Buttons */}
         <div className="flex items-center justify-center my-6">
-          <Link to="/signup">
-            <ButtonAsset
-              text="취소"
-              variant="outlined"
-              size="lg"
-              className="mr-9"
-            />
-          </Link>
+          <ButtonAsset
+            text="취소"
+            variant="outlined"
+            size="lg"
+            className="mr-9"
+            onClick={handleCancelButton}
+          />
           <ButtonAsset
             type="submit"
             text="회원 가입"
--- front/src/store/useRegistStore.tsx
+++ front/src/store/useRegistStore.tsx
@@ -1,5 +1,9 @@
 import { create } from "zustand";
-import { getDuplicateCheckId } from "../apis/api/member";
+import {
+  getDuplicateCheckEmail,
+  getDuplicateCheckId,
+  getDuplicateCheckNickname,
+} from "../apis/api/member";
 
 class TextFieldError extends Error {
   constructor(message: string, name: string = "error") {
@@ -22,9 +26,21 @@ interface RegistState {
   passwordConfirmError: TextFieldError;
   setPasswordConfirm: (by: string) => void;
 
+  email: string;
+  emailError: TextFieldError;
+  setEmail: (by: string) => void;
+
+  nickname: string;
+  nicknameError: TextFieldError;
+  setNickname: (by: string) => void;
+
   checkId: () => Promise<boolean>;
   checkPassword: () => boolean;
   checkPasswordConfirm: () => boolean;
+  checkEmail: () => Promise<boolean>;
+  checkNickname: () => Promise<boolean>;
+
+  getRegistValid: () => Promise<boolean>;
 }
 
 const useRegistStore = create<RegistState>((set, get) => ({
@@ -39,6 +55,14 @@ const useRegistStore = create<RegistState>((set, get) => ({
   passwordConfirm: "",
   passwordConfirmError: new Error(),
   setPasswordConfirm: (by) => set(() => ({ passwordConfirm: by })),
+
+  email: "",
+  emailError: new Error(),
+  setEmail: (by) => set(() => ({ email: by })),
+
+  nickname: "",
+  nicknameError: new Error(),
+  setNickname: (by) => set(() => ({ nickname: by })),
 
   // 아이디 유효성 검사
   checkId: async () => {
@@ -62,7 +86,7 @@ const useRegistStore = create<RegistState>((set, get) => ({
     }
 
     // 중복된 아이디인 경우
-    // const isDuplicate = await getDuplicateCheckId(value);
+    // const isDuplicate = await getDuplicateCheckId(id);
     const isDuplicate = false; // WARNING: API 완성되면 위의 코드로 교체해주세요.
     if (isDuplicate) {
       error.message = "이미 사용 중인 아이디입니다.";
@@ -133,6 +157,97 @@ const useRegistStore = create<RegistState>((set, get) => ({
     error.message = "";
     set(() => ({ passwordConfirmError: error }));
     return true;
+  },
+
+  // 이메일 유효성 검사
+  checkEmail: async () => {
+    const email = get().email;
+    const error = new TextFieldError("");
+
+    // 입력값이 없을 경우
+    if (!email) {
+      error.message = "이메일을 입력해주세요.";
+      set(() => ({ emailError: error }));
+      return false;
+    }
+
+    // 이메일 형식이 아닌 경우
+    const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-z]+$/;
+    if (!emailRegex.test(email)) {
+      error.name = "error";
+      error.message = "이메일 형식이 옳바르지 않습니다.";
+      set(() => ({ emailError: error }));
+      return false;
+    }
+
+    // 중복된 이메일인 경우
+    // const isDuplicate = await getDuplicateCheckEmail(email);
+    const isDuplicate = false; // WARNING: API 완성되면 위의 코드로 교체해주세요.
+    if (isDuplicate) {
+      error.message = "이미 사용 중인 아이디입니다.";
+      set(() => ({ idError: error }));
+      return false;
+    }
+
+    // 모든 검사 통과!
+    error.name = "valid";
+    error.message = "";
+    set(() => ({ emailError: error }));
+    return true;
+  },
+
+  // 닉네임 유효성 검사
+  checkNickname: async () => {
+    const nickname = get().nickname;
+    const error = new TextFieldError("");
+
+    // 입력값이 없을 경우
+    if (!nickname) {
+      error.message = "닉네임을 입력해주세요.";
+      set(() => ({ nicknameError: error }));
+      return false;
+    }
+
+    // 조건에 맞지 않은 경우
+    // 공백 없이 영어, 한글, 숫자만 입력, 2글자 이상, 10글자 이하
+    const nickNameRegex = /^[가-힣a-zA-Z\d]{2,10}$/;
+    if (!nickNameRegex.test(nickname)) {
+      error.message = "닉네임은 2 ~ 10자의 한글, 영문, 숫자만 입력 가능합니다.";
+      set(() => ({ nicknameError: error }));
+      return false;
+    }
+
+    // 중복된 닉네임인 경우
+    // const isDuplicate = await getDuplicateCheckNickname(nickname);
+    const isDuplicate = false; // WARNING: API 완성되면 위의 코드로 교체해주세요.
+    if (isDuplicate) {
+      error.message = "이미 사용 중인 닉네임입니다.";
+      set(() => ({ nicknameError: error }));
+      return false;
+    }
+
+    // 모든 검사 통과!
+    error.name = "valid";
+    error.message = "";
+    set(() => ({ nicknameError: error }));
+    return true;
+  },
+
+  // 모든 폼이 유효한지 확인
+  getRegistValid: async () => {
+    const idCheck = await get().checkId();
+    const passwordCheck = get().checkPassword();
+    const passwordConfirmCheck = get().checkPasswordConfirm();
+    const emailCheck = await get().checkEmail();
+    const nicknameCheck = await get().checkNickname();
+
+    return (
+      idCheck &&
+      passwordCheck &&
+      passwordConfirmCheck &&
+      emailCheck &&
+      nicknameCheck
+    );
   },
 }));
 
