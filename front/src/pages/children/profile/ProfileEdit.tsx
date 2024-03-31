32da8650e0ec4ed754ac8fc2de30627b44f81f69
임지은 <ljieun01@naver.com>
2024-03-31 19:41:10 +0900

    Feat: 프로필 수정 페이지 기능 추가 - API 대기중

--- front/src/pages/children/profile/ProfileEdit.tsx
+++ front/src/pages/children/profile/ProfileEdit.tsx
@@ -6,46 +6,43 @@ import ButtonAsset from "../../../components/Button/ButtonAsset";
 import InputAsset from "../../../components/common/InputAsset";
 
 export default function ProfileEdit() {
-  const imageList = Array.from({ length: 8 }, (v, i) => i);
-  const [ProfileData, setProfileData] = useState({
-    profileImageIndex: 0,
-    nickname: "",
-    email: "",
-  });
-  // TODO usestate의 기본값을 api get으로 읽어오거나, zustand로 처리하거나.
+  const imageIndexList = Array.from({ length: 8 }, (v, i) => i);
 
-  const { Id } = useParams();
-
-  const [index, setIndex] = useState(0);
+  const [profileIndex, setProfileIndex] = useState(0);
   const [nickname, setNickname] = useState("");
   const [email, setEmail] = useState("");
 
+  const { memberId } = useParams();
   const navigate = useNavigate();
 
   const handleClick = () => {
-    setProfileData((ProfileData) => ({
-      ...ProfileData,
-      nickname: nickname,
-      profileImageIndex: index,
-      email: email,
-    }));
-    navigate(`/mypage/${Id}/profile/`);
-    console.log(ProfileData);
+    try {
+      // TODO: 회원 정보 수정 API 추가
+      alert("회원 정보가 수정되었습니다!");
+      navigate(`/mypage/${memberId}/profile/`);
+    } catch (error) {
+      console.error(error);
+    }
   };
+
   useEffect(() => {
-    setProfileData((ProfileData) => ({
-      ...ProfileData,
-      nickname: nickname,
-      profileImageIndex: index,
-      email: email,
-    }));
-  }, [nickname, index, email]);
+    // TODO: 회원 조회 API 추가
+    const data = {
+      nickname: "ssafy1",
+      email: "ssafy1@ssafy.com",
+      profileImage: 1,
+    };
+
+    setProfileIndex(data.profileImage);
+    setNickname(data.nickname);
+    setEmail(data.email);
+  }, []);
 
   return (
     <div className="w-[600px]">
       <div className="flex flex-col w-full p-4 my-10 border rounded-md">
         <div className="flex my-3">
-          <Link to={`/mypage/1/profile/`} className="pr-3">
+          <Link to={`/mypage/${memberId}/profile/`} className="pr-3">
             <ArrowBackIcon fontSize="large" />
           </Link>
           <h1 className="text-2xl font-bold">회원 정보 수정</h1>
@@ -54,20 +51,20 @@ export default function ProfileEdit() {
           <h2 className="mb-3 text-lg font-bold">프로필 사진</h2>
           <div className="flex w-full">
             <img
-              src={`/test/picture${index}.jpg`}
+              src={`/test/picture${profileIndex}.jpg`}
               alt="empty"
               className="mr-3 w-36 h-36"
             />
             <div className="flex flex-wrap justify-between content-between w-[300px] h-36">
-              {imageList.map((img) => (
+              {imageIndexList.map((imgIdx) => (
                 <img
-                  key={img}
-                  src={`/test/picture${img}.jpg`}
+                  key={imgIdx}
+                  src={`/test/picture${imgIdx}.jpg`}
                   alt="프로필 사진을 지정하세요"
                   className={`w-16 h-16 ${
-                    index === img && "border-2 border-primary box-border"
+                    profileIndex === imgIdx && "border-[3px] border-primary"
                   }`}
-                  onClick={() => setIndex(img)}
+                  onClick={() => setProfileIndex(imgIdx)}
                 />
               ))}
             </div>
@@ -75,18 +72,26 @@ export default function ProfileEdit() {
         </div>
         <div className="w-full py-5 border-b">
           <h2 className="mb-3 text-lg font-bold">닉네임</h2>
-          <InputAsset value="" placeholder="닉네임" className="w-[300px]" />
+          <InputAsset
+            value={nickname}
+            placeholder="닉네임"
+            className="w-[300px]"
+          />
         </div>
         <div className="w-full py-5">
           <h2 className="mb-3 text-lg font-bold">이메일</h2>
-          <InputAsset value="" placeholder="이메일" className="w-[300px]" />
+          <InputAsset
+            value={email}
+            placeholder="이메일"
+            className="w-[300px]"
+          />
         </div>
         <div className="flex justify-center w-full my-5">
           <ButtonAsset
             text="취소"
             variant="outlined"
             className="w-24 mr-8"
-            onClick={() => navigate("/mypage/1/profile/")}
+            onClick={() => navigate(`/mypage/${memberId}/profile/`)}
           />
           <ButtonAsset text="저장" className="w-24" onClick={handleClick} />
         </div>
