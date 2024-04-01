7596540ee23c76033fea926fbe22512d17190bf1
우지민 <0542say@naver.com>
2024-04-01 17:45:23 +0900

    Feat: api 맞춰서 param 수정

--- front/src/components/card/FoodResultCard.tsx
+++ front/src/components/card/FoodResultCard.tsx
@@ -1,31 +1,30 @@
 import { NavLink } from "react-router-dom";
-interface CardProps {
-  food: string;
-  category: string;
-  src?: string;
-}
-export default function FoodResultCard(props: CardProps) {
+
+export default function FoodResultCard(props: {
+  menu_id: number;
+  menu_name: string;
+  cate_image: number;
+}) {
   return (
-    <NavLink
-      // className="flex flex-col justify-around w-full p-2 m-2 border-2 border-gray-300 shadow-md h-[60vh] rounded-xl"
-      className="flex flex-col justify-between w-[18vw] p-2 m-2 border-2 border-gray-300 shadow-md h-[45vh] rounded-2xl hover:border-primary"
-      to={`/`}
-    >
+    // <NavLink // to={`/`}
+    <div className="flex flex-col justify-between w-[18vw] p-2 m-2 border-2 border-gray-300 shadow-md h-[45vh] rounded-2xl hover:border-primary">
       <div className="flex justify-center w-full grow">
         <img
-          src={props.src}
-          alt={props.food}
+          src="/menu_test/korean.png"
+          alt={props.menu_name}
           className="w-3/4"
         />
       </div>
       <div className="flex flex-col justify-center">
         <div className="flex justify-center m-3 text-2xl font-semibold">
-          {props.food}
+          {props.menu_name}
         </div>
         <div className="flex justify-center m-3 font-medium text-gray-500">
-          {props.category}
+          {/* {props.category} */}
+          한식
         </div>
       </div>
-    </NavLink>
+      {/* </NavLink> */}
+    </div>
   );
 }
--- front/src/components/card/MenuCard.tsx
+++ front/src/components/card/MenuCard.tsx
@@ -8,23 +8,26 @@ export default function MenuCard(props: {
   menu_id: number;
   menu_name: string;
   cate_image: number;
+  className?: string;
 }) {
   return (
-    <div className="flex flex-col justify-between w-full h-full p-3 border-2 border-gray-300 shadow-md rounded-xl ">
+    <div
+      className={`flex flex-col justify-between w-full h-full p-3 border-2 border-gray-300 shadow-md rounded-xl ${props.className}`}
+    >
       <div className="flex justify-center w-full">
         <img
           src="/menu_test/korean.png"
-          alt="empty"
+          alt={props.menu_name}
           className="flex justify-center w-[65%]"
         />
-        {/* TODO 이미지 링크, 메뉴명, 분류 넣기 */}
       </div>
       <div className="flex flex-col justify-center">
         <div className="flex justify-center m-1 text-xl font-semibold">
           {props.menu_name}
         </div>
         <div className="flex justify-center mb-2 font-medium text-gray-500">
-          {props.menu_id}
+          {/* {props.menu_id} */}
+          한식
         </div>
       </div>
     </div>
