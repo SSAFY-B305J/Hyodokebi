7e3f25179ecf71f02de238f3325c74daedb0c72a
전성수 <ke_wer@naver.com>
2024-04-01 15:08:39 +0900

    Fix: kakao 로그인 오류 수정

--- Back/dokebi/src/main/java/com/dokebi/dokebi/common/config/CorsConfig.java
+++ Back/dokebi/src/main/java/com/dokebi/dokebi/common/config/CorsConfig.java
@@ -13,7 +13,6 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**").allowedOrigins("*")
-
                 .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
                         HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(),
                         HttpMethod.PATCH.name())
