b916873e21c7d13c35ed92257e6bc4ad27948ca1
우지민 <0542say@naver.com>
2024-04-02 14:27:02 +0900

    Update Back/dokebi/src/main/java/com/dokebi/dokebi/common/config/CorsConfig.java

--- Back/dokebi/src/main/java/com/dokebi/dokebi/common/config/CorsConfig.java
+++ Back/dokebi/src/main/java/com/dokebi/dokebi/common/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
-        registry.addMapping("/**").allowedOrigins("*")
+        registry.addMapping("/**").allowedOrigins("j10b305.p.ssafy.io")
                 .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
                         HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(),
                         HttpMethod.PATCH.name())
