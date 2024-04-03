7b39caccddcc76f775874a18468ff7f914925d5c
임지은 <ljieun01@naver.com>
2024-04-03 21:38:47 +0900

    Chore: 카카오 로그인 redirect url 수정

--- front/src/modules/auth/kakaoAuth.ts
+++ front/src/modules/auth/kakaoAuth.ts
@@ -1,3 +1,4 @@
 export const REST_API_KEY = process.env.REACT_APP_KAKAO_OAUTH_KEY;
-export const REDIRECT_URI = "http://localhost:3000/api/member/login/kakao";
+export const REDIRECT_URI =
+  "https://hyodokebi.duckdns.org/api/member/login/kakao";
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
