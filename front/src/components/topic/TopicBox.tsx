722b4901d252055a7a7a120948fae034ebb64ced
임지은 <ljieun01@naver.com>
2024-04-04 01:47:41 +0900

    Feat: 대화주제 json 추가, 대화 주제 추천 css 수정

--- front/src/components/topic/TopicBox.tsx
+++ front/src/components/topic/TopicBox.tsx
@@ -2,17 +2,21 @@ import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import useRecommendStore from "../../store/useRecommendStore";
 import ButtonAsset from "../Button/ButtonAsset";
+import topicData from "../../json/TalkTopic.json";
+import { CircularProgress, Tooltip } from "@mui/material";
+import { ContentCopy } from "@mui/icons-material";
 
 interface TopicBoxProps {
   isLogin: boolean;
 }
 
 export default function TopicBox({ isLogin }: TopicBoxProps) {
-  const { recom, setRecom } = useRecommendStore();
-  const [isLoading, setIsLoading] = useState(true); // 페이지 로딩 시 항상 로딩 상태로 설정
+  // const { recom, setRecom } = useRecommendStore(); // 이전 질문 저장, 중복 방지
+  const [isLoading, setIsLoading] = useState(false); // 페이지 로딩 시 항상 로딩 상태로 설정
   const navigate = useNavigate();
-  const topicshow = Math.floor(Math.random() * 10);
-  const show = Math.floor(Math.random() * 10);
+
+  const [isResultScreen, setIsResultScreen] = useState(false);
+
   const clickToClipboard = async (text: string) => {
     try {
       await navigator.clipboard.writeText(text);
@@ -21,12 +25,18 @@ export default function TopicBox({ isLogin }: TopicBoxProps) {
       alert("복사에 실패했습니다.");
     }
   };
-  const getContent = () => {
-    return document.getElementById("question")?.innerHTML ?? "";
-  };
 
-  const randomtopictest = `testtopic${topicshow}`;
-  const randomtest = `test${show}`;
+  const getRandomTopicIndex = () =>
+    Math.floor(Math.random() * topicData.length);
+
+  const [topicIndex, setTopicIndex] = useState(0);
+  const randomTopic = topicData[topicIndex];
+
+  const initTopic = () => {
+    sLoading();
+    setIsResultScreen(true);
+    setTopicIndex(getRandomTopicIndex());
+  };
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -40,66 +50,57 @@ export default function TopicBox({ isLogin }: TopicBoxProps) {
     setIsLoading(true);
     setTimeout(() => {
       setIsLoading(false);
-      setRecom(true);
     }, 1000);
   };
-  const radnum: number = new Date().getMilliseconds() % 4;
 
   return (
-    <div className="w-[650px] h-[350px] bg-secondary border-secondary border-2 rounded-3xl p-3 flex">
-      {isLoading ? (
-        <div className="flex flex-col items-center justify-center w-full h-full">
-          <img
-            src={`/loading_illust/loading_${radnum}.png`}
-            className="w-[256px] h-[256px]"
-            alt="empty"
-          />
-          <div className="text-xl">로딩중...</div>
+    <div className="border-2 border-lightsilver rounded-md w-[550px] min-h-[300px] py-10 flex flex-col justify-center items-center">
+      {!isResultScreen && (
+        <div className="flex flex-col items-center">
+          <h1 className="mb-10 text-3xl font-semibold">오늘의 대화 주제는?</h1>
+          <ButtonAsset text="대화 주제 추천 받기" onClick={initTopic} />
         </div>
-      ) : recom ? (
-        <div className="flex flex-col items-center justify-around w-full h-full">
-          <div className="text-3xl font-semibold">
-            오늘은 이 주제로 대화해보세요.
-          </div>
-          <div className="text-2xl">{randomtopictest}</div>
-          <div className="flex flex-col items-center justify-center w-3/4 gap-3 bg-white h-1/6 rounded-3xl">
-            <div
-              className="flex justify-center text-2xl cursor-pointer"
-              id="question"
-              onClick={() => {
-                clickToClipboard(getContent());
-              }}
-            >
-              {randomtest}
+      )}
+
+      {isResultScreen && isLoading && (
+        <div className="flex flex-col items-center my-8">
+          <CircularProgress color="warning" />
+          <p className="my-3">효도깨비가 고민중...</p>
+        </div>
+      )}
+      {isResultScreen && !isLoading && (
+        <div className="flex flex-col items-center">
+          <div className="text-center text-gray-500">오늘의 대화 주제</div>
+          <div className="my-8">
+            <h3 className="mb-8 text-3xl font-semibold text-center">
+              {randomTopic.topic}
+            </h3>
+            <div className="flex items-center whitespace-pre-wrap">
+              <div className="w-[400px] bg-gray-100 text-center p-3 rounded-md">
+                {randomTopic.question}
+              </div>
+              <Tooltip title="복사" arrow>
+                <button
+                  className="ml-3"
+                  onClick={() => clickToClipboard(randomTopic.question)}
+                >
+                  <ContentCopy />
+                </button>
+              </Tooltip>
             </div>
-            <div className="flex text-xs">클릭하면 복사됩니다.</div>
           </div>
           {isLogin ? (
-            <ButtonAsset
-              text="다시 추천 받기"
-              onClick={() => {
-                sLoading();
-              }}
-            />
+            <ButtonAsset text="다시 추천 받기" onClick={initTopic} />
           ) : (
             <ButtonAsset
               text="비회원은 로그인 후 재추천 가능합니다."
+              variant="outlined"
               className="p-3 text-sm bg-gray-400 border-gray-300 rounded-md"
               onClick={() => {
                 navigate("/login");
               }}
             />
           )}
-        </div>
-      ) : (
-        <div className="flex flex-col items-center justify-around w-full h-full">
-          <div className="text-2xl">대화 주제를 추천 해드립니다!</div>
-          <ButtonAsset
-            onClick={() => {
-              sLoading(); // 로딩 시작
-            }}
-            text="추천 받기"
-          />
         </div>
       )}
     </div>
